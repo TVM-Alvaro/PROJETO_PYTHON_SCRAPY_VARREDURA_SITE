1abb0ca6c1173d898004fc4eddae0bfd3b8ca56d
Thiago Alvaro <alvaropmes@gmail.com>
2025-03-13 11:12:45 -0300

    versao 2

---
+++ varredor_de_sites/varredor_de_sites/spiders/goodreads.py
@@ -0,0 +1,27 @@
+import scrapy
+
+# CamelCase
+
+
+class QuotesToScrapeSpider(scrapy.Spider):
+    # Identidade
+    name = 'quotebot'
+    # Request
+
+    def start_requests(self):
+        # Definir url(s) a varrer
+        urls = ['https://www.goodreads.com/quotes']
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    # Response
+
+    def parse(self, response):
+        # aqui é onde você deve processar o que é retornado da response
+        for elemento in response.xpath("//div[@class='quote']"):
+            yield {
+                'frase': elemento.xpath(".//div[@class='quoteText']/text()").get(),
+                'autor': elemento.xpath(".//span[@class='authorOrTitle']/text()").get(),
+                # para achar as tags foi usado um xpath composto.
+                'tags': elemento.xpath(".//div[@class='greyText smallText left']/a/text()").getall()
+            }
